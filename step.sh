dc87b6031912e766b697cacb65fa8cde89160765
Ian Cowley <ian@tidelaundry.com>
2018-12-11 09:55:08 -0600

    Testing initial setup.

--- step.sh
+++ step.sh
@@ -1,22 +1,50 @@
 #!/bin/bash
-set -ex
 
-echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
+# exit if a command fails
+set -e
 
 #
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
+# Required parameters
+if [ -z "${app_path}" ] ; then
+  echo " [!] Missing required input: app_path"
+  exit 1
+fi
+if [ ! -f "${app_path}" ] ; then
+  echo " [!] File doesn't exist at specified path: ${app_path}"
+  exit 1
+fi
+if [ -z "${app_center_app}" ] ; then
+  echo " [!] Missing required input: app_center_app"
+  exit 1
+fi
+if [ -z "${app_center_distribution_group}" ] ; then
+  echo " [!] Missing required input: app_center_distribution_group"
+  exit 1
+fi
+if [ -z "${app_center_token}" ] ; then
+  echo " [!] Missing required input: app_center_token"
+  exit 1
+fi
+
+
+# ---------------------
+# --- Configs:
+
+echo " (i) Provided app path: ${app_path}"
+echo " (i) Provided app center app: ${app_center_app}"
+echo " (i) Provided app center distribution group: ${app_center_distribution_group}"
+echo " (i) Provided app center token: ********"
+echo
+
+# ---------------------
+# --- Main
+
+npm install appcenter-cli@1.0.8 -g
+appcenter distribute release --file "$app_path" --app "$app_center_app" --group "$app_center_distribution_group" --token "${app_center_token}"
 
 #
 # --- Exit codes:
 # The exit code of your Step is very important. If you return
 #  with a 0 exit code `bitrise` will register your Step as "successful".
 # Any non zero exit code will be registered as "failed" by `bitrise`.
+
